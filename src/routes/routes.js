9c5639b3390531d5db246ee90e70438942aa9e49
DPLakmal <pubudu.lakmalc@gmail.com>
2024-04-25 23:23:34 +0530

    feat: Implement subject get function

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import User from '../models/user.js'
+import Subject from '../models/subject.js'
 
 const router = express.Router()
 
@@ -19,6 +20,29 @@ router.post('/register', async (req, res) => {
     const user = User({ username, password })
     const newUser = await user.save()
     res.status(201).json(newUser)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+router.get('/login', async (req, res) => {
+  const username = req.body
+
+  try {
+    // Check if username already exists
+    const existingUser = await User.findById({ username })
+    res.json(existingUser)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+//sem 1 all subject
+
+router.get('/sem1', async (req, res) => {
+  try {
+    const subject = await Subject.find()
+    res.json(subject)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
