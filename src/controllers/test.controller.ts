39c3bb082121b871fe17b54f9855c4031f56b7e3
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-01 18:41:00 +0530

    refactor: empty file delete

--- src/controllers/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import axios, { Axios, AxiosResponse } from 'axios';
-import { Request, Response, NextFunction } from 'express';
-
-
-const testFunction = async (req: Request, res: Response, next: NextFunction) => {
-    let result: AxiosResponse = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`);
-    return res.send(result.data);
-}
-
