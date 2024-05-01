b067a64ae53953e3d1b7906674debd46d8d79f9c
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-01 20:01:23 +0530

    chore: Separate express imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import router from './routes/routes';
