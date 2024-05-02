653b774515bfc2809e7cb713afb59ae11ba851b2
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-02 21:34:04 +0530

    feat: Add semester schema

---
+++ src/models/semesters.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+const { Schema, models } = mongoose
+
+const subjectSchema = new Schema({
+  code: String,
+  title: String,
+  coverImg: String,
+  credit: Number,
+})
+
+const semesterSchema = new Schema({
+  semester: Number,
+  subjects: [subjectSchema],
+})
+
+const Semesters = mongoose.model('Semesters', semesterSchema)
+export default Semesters
